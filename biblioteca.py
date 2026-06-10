f4f075dea263e550e2847c7b831f904ff26335a5
ArthurVianna-D <it.arthurplanski@gmail.com>
2026-06-09 23:49:11 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -15,3 +15,27 @@ class Livro:
             f'Status: {status}'
         )
 
+class Biblioteca:
+    def __init__(self):
+        self.livros = []
+
+    def adicionar_livro(self, livro):
+        self.livros.append(livro)
+
+    def listar_livros(self):
+        print('\n===== CATÁLOGO =====')
+
+        if not self.livros:
+            print('Nenhum livro cadastrado.')
+            return
+
+        for livro in self.livros:
+            print('-' * 30)
+            print(livro)
+
+    def buscar_por_codigo(self, codigo):
+        for livro in self.livros:
+            if livro.codigo == codigo:
+                return livro
+
+        return None
